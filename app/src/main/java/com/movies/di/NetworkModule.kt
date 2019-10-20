946f426fc95d0025f2a5e72260bf895c776b1dd8
kk2013 <jo.x.santamaria@gmail.com>
2019-10-20 22:54:16 +0900

    Retrieving movies

--- app/src/main/java/com/movies/di/NetworkModule.kt
+++ app/src/main/java/com/movies/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import com.movies.api.MoviesService
 import com.movies.utils.LiveDataCallAdapterFactory
 import dagger.Module
 import dagger.Provides
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,16 +16,30 @@ import javax.inject.Singleton
 @Module
 class NetworkModule {
 
-    private val BASE_URL = "http://api.icndb.com"
+    private val BASE_URL = "https://api.themoviedb.org"
 
     private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
         this.level = if(BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.BASIC
+    }
+
+    private fun provideHeaderInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val request = chain.request()
+            val builder = request.newBuilder()
+            val url = request.url
+                .newBuilder()
+                .addQueryParameter("api_key", "9228b5e724e8040388b1f2590732462b")
+                .build()
+            builder.url(url)
+            chain.proceed(builder.build())
+        }
     }
 
     @Provides
     @Singleton
     fun providesOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
         .addInterceptor(interceptor)
+        .addInterceptor(provideHeaderInterceptor())
         .connectTimeout(2, TimeUnit.SECONDS)
         .readTimeout(2, TimeUnit.SECONDS)
         .build()
