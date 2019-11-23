94bceabfbaa7fbc4a6798d5b4ed6981265f34374
kk2013 <jo.x.santamaria@gmail.com>
2019-11-23 17:33:46 +0000

    Delete redundant class

--- app/src/main/java/com/movies/moviedetails/MovieDetailsViewModel.kt
+++ app/src/main/java/com/movies/moviedetails/MovieDetailsViewModel.kt
@@ -6,25 +6,23 @@ import androidx.lifecycle.viewModelScope
 import com.chuck.util.wrapEspressoIdlingResource
 import com.movies.data.MoviesRepository
 import com.movies.model.Movie
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MovieDetailsViewModel @Inject constructor(private val movieDetailsRepository: MoviesRepository) : ViewModel() {
+class MovieDetailsViewModel @Inject constructor(private val movieDetailsRepository: MoviesRepository) :
+    ViewModel() {
 
-    private val _state = MutableLiveData<MovieDetailsState>()
-    val state: MutableLiveData<MovieDetailsState>
-        get() = _state
+    internal val state = MutableLiveData<MovieDetailsState>()
 
     fun getMovieDetails(movieId: String) = viewModelScope.launch {
 
-        _state.value = MovieDetailsState.Loading
+        state.value = MovieDetailsState.Loading
         wrapEspressoIdlingResource {
             try {
                 val movieDetails = movieDetailsRepository.getMovieDetails(movieId)
-                _state.value = MovieDetailsState.Success(movieDetails)
+                state.value = MovieDetailsState.Success(movieDetails)
             } catch (ex: Exception) {
-                _state.value = MovieDetailsState.Failed
+                state.value = MovieDetailsState.Failed
             }
         }
     }
